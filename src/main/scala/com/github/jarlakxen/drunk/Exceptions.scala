d9c3274c42b2367298a8349854833898a2cb55d8
Piotr Trzpil <piotr.trzpil@gmail.com>
2019-02-08 11:41:45 +0000

    fix exception

--- src/main/scala/com/github/jarlakxen/drunk/Exceptions.scala
+++ src/main/scala/com/github/jarlakxen/drunk/Exceptions.scala
@@ -1,3 +1,3 @@
 package com.github.jarlakxen.drunk
 
-class NonOkHttpCodeException(code: Int, body: String) extends Exception(s"Received http code: $code with body: $body")
+class NonOkHttpCodeException(val code: Int, val body: String) extends Exception(s"Received http code: $code with body: $body")
